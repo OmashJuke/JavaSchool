f6cabebebdca12a945f9e2a107ee5cb646e856bc
oleg <oleg-mashenko@rambler.ru>
2019-03-03 23:14:10 +0300

    initial project version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("hello");
+        System.out.println("hello");
+        Person oleg = new Person(true, "Oleg");
+        Person july = new Person(false, "July");
+        Person igor = new Person(true, "Igor");
+        Person kate = new Person(false, "Kate");
+        System.out.println(oleg.marry(july));
+        System.out.println(oleg.marry(igor));
+        System.out.println(oleg.marry(kate));
+        System.out.println(oleg.GetSpouseName());
+        System.out.println(kate.GetSpouseName());
+
+
     }
 }
--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -1,4 +1,60 @@
 package com.company;
 
 public class Person {
+    private final boolean man;
+    private final String name;
+    private Person spouse;
+
+    public Person(boolean man, String name) {
+        this.man = man;
+        this.name = name;
+    }
+    public String GetName(){
+        return name;
+    }
+    public String GetSpouseName(){
+        return spouse.name;
+    }
+
+
+    public boolean marry(Person person) {
+        if (this.man && person.man) {
+            return false;
+        } else {
+            if (isFamily(person)) {
+                return false;
+            } else {
+                this.spouse=person;
+                person.spouse=this;
+                return true;
+            }
+        }
+
+
+    }
+
+    public boolean isFamily(Person person) {
+        if (spouse==null) {
+            return false;
+        } else {
+            if ((this==person.spouse) && (this.spouse==person)) {
+                return true;
+            } else {
+
+                return false;
+            }
+        }
+    }
+
+    public boolean divorce() {
+        if (spouse==null) {
+            return false;
+        } else {
+            spouse.spouse = null;
+            this.spouse = null;
+            return true;
+        }
+    }
+
+
 }
